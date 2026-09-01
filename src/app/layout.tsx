d7c0c24d6b385ac9266c8524bb46851a99b5942f
mahya.mofrad <mhya.mofrad@gmail.com>
2026-09-01 23:25:36 +0330

    feat(Laout.tsx): add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,35 @@
 import "./globals.css";
-import { Geist } from "next/font/google";
-import { cn } from "@/src/lib/utils";
+import localFont from "next/font/local";
 import Footer from "@/src/components/shared components/Footer";
 import Navbar from "@/src/components/shared components/Navbar";
 
-const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
+const yekanBakh = localFont({
+  src: [
+    {
+      path: "../../public/fonts/woff2/YekanBakhFaNum-Light.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/woff2/YekanBakhFaNum-SemiBold.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/woff2/YekanBakhFaNum-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-fa",
+  display: "swap",
+  preload: true,
+});
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="fa" dir="rtl" className={cn("font-sans", geist.variable)}>
-      <body>
+    <html lang="fa" dir="rtl" className={yekanBakh.variable}>
+      <body className="font-[var(--font-fa)]">
         <main className="flex min-h-screen flex-col">
           <div className="h-1/5 w-full flex flex-col items-center py-4">
             <Navbar />
