87cf1498437c2eabcce48cdb8604c1b1e43b07fa
mahya.mofrad <mhya.mofrad@gmail.com>
2026-09-04 23:28:42 +0330

    feat(Footer): add social media accounts

--- src/components/shared components/Footer.tsx
+++ src/components/shared components/Footer.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+import { Instagram, LinkedIn, Telegram } from "../icons/SocialMedia";
 import { Separator } from "../ui/separator";
 
 export default function Footer() {
   return (
-    <section className="w-full grid grid-flow-col justify-around bg-footer-bg p-10">
+    <footer className="w-full grid grid-flow-col justify-around bg-footer-bg p-10">
       <div className="grid-cols-4">
         <div className="hidden flex-col gap-1 md:flex">
           <span className="font-medium text-amber-950">Help</span>
@@ -19,7 +21,7 @@ export default function Footer() {
           </div>
           <Separator orientation="vertical" className="bg-muted-foreground" />
           <div className="flex flex-col gap-1">
-            <span className="font-medium text-amber-950">Account</span>
+            <span className="font-medium text-amber-950">tell</span>
             <span className="text-xs text-muted-foreground">
               Profile & security
             </span>
@@ -29,13 +31,24 @@ export default function Footer() {
             className="hidden md:block bg-muted-foreground"
           />
           <div className="hidden flex-col gap-1 md:flex">
-            <span className="font-medium text-amber-950">Help</span>
-            <span className="text-xs text-muted-foreground">
-              Support & docs
+            <span className="font-medium text-amber-950">Social Media</span>
+            <span className="flex gap-3 text-xs text-icon-color">
+              <Link
+                href="https://www.linkedin.com/in/mahya-mofrad"
+                target="_blank"
+              >
+                <LinkedIn className="w-4 h-4" />
+              </Link>
+              <Link href="https://t.me/mahya_shm" target="_blank">
+                <Telegram className="w-4 h-4" />
+              </Link>
+              <Link href="https://www.instagram.com/mhya_shm" target="_blank">
+                <Instagram className="w-4 h-4" />
+              </Link>
             </span>
           </div>
         </div>
       </div>
-    </section>
+    </footer>
   );
 }
