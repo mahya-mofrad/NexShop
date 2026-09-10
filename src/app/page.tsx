79cb4dad1e51ebe678f3c0be16be37fbb41d2771
mahya.mofrad <mhya.mofrad@gmail.com>
2026-09-10 22:08:26 +0330

    fix(banners): rtl fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,17 @@ import Banners from "@/src/components/features/Banners";
 
 export default function HomePage() {
   return (
-    <main className="w-dvw h-dvh">
-      <section className="h-1/3">
+    <main>
+      <section className="w-full">
         <Banners />
+      </section>
+      <section className="w-full h-[50vh] bg-primary-foreground">
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-4xl font-bold mb-4">Welcome to Our Store</h1>
+          <p className="text-lg text-gray-700">
+            Discover our amazing products and offers.
+          </p>
+        </div>
       </section>
     </main>
   );
--- src/components/features/Banners.tsx
+++ src/components/features/Banners.tsx
@@ -3,7 +3,9 @@
 import * as React from "react";
 import Autoplay from "embla-carousel-autoplay";
 
-import bannerItem from "@/public/images/ChatGPT Image Aug 15, 2026, 05_17_40 PM.png";
+import bannerItem1 from "@/public/images/banner.png";
+import bannerItem2 from "@/public/images/ChatGPT Image Aug 15, 2026, 05_17_40 PM.png";
+import bannerItem3 from "@/public/images/nexShopLOGO.png";
 
 import {
   Carousel,
@@ -14,35 +16,39 @@ import Image from "next/image";
 
 export default function CarouselPlugin() {
   const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true }),
+    Autoplay({ delay: 2000, stopOnMouseEnter: true }),
   );
 
   return (
     <Carousel
-      plugins={[plugin.current]}
-      className="w-auto h-auto"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
+      dir="rtl"
       opts={{
         loop: true,
+        direction: "rtl",
       }}
+      plugins={[plugin.current]}
+      className="w-screen"
     >
       <CarouselContent>
-        <CarouselItem>
-          <div className="text-amber-400 text-4xl">
-            <Image src={bannerItem} alt={"Banner"} />1
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <div className="text-amber-400 text-4xl">
-            <Image src={bannerItem} alt={"Banner"} />2
-          </div>
-        </CarouselItem>
-        <CarouselItem>
-          <div className="text-amber-400 text-4xl">
-            <Image src={bannerItem} alt={"Banner"} />3
-          </div>
-        </CarouselItem>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="relative min-w-screen h-[43.333vh] overflow-hidden">
+              <Image
+                src={
+                  index === 0
+                    ? bannerItem3
+                    : index === 1
+                      ? bannerItem2
+                      : bannerItem1
+                }
+                alt={`Banner ${index + 1}`}
+                sizes="100vw"
+                className="object-cover"
+                fill
+              />
+            </div>
+          </CarouselItem>
+        ))}
       </CarouselContent>
     </Carousel>
   );
